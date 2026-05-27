d11bf93ba24f7016f79d2e17d2d2d87ca9849b27
Fredy Sandoval <shockerovip@gmail.com>
2026-05-26 23:30:14 -0600

    add: scripts/setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,38 @@
+sudo pacman -S --needed --noconfirm   \
+fnm       \
+pnpm      \
+git-delta \
+pass      \
+pass-otp  \
+rustup    \
+go        \
+lazygit   \
+dolphin   \
+tmux      \
+starship  \
+stow      \
+
+paru -S --needed --noconfirm --removemake neovim-nightly google-chrome
+
+if ! rustup show active-toolchain >/dev/null 2>&1; then
+  rustup default stable
+fi
+
+files=(
+  "/usr/lib/browserpass/hosts/chromium/com.github.browserpass.native.json"
+)
+
+for file in "${files[@]}"; do
+  if [ -e "$file" ]; then
+    # browserpass
+    sudo mkdir -p /etc/opt/chrome/native-messaging-hosts
+    sudo ln -sf /usr/lib/browserpass/hosts/chromium/com.github.browserpass.native.json \
+      /etc/opt/chrome/native-messaging-hosts/com.github.browserpass.native.json
+  fi
+done
+
+# Blender GPU support
+# sudo pacman -S hip-runtime-amd \
+# 	  hiprt                  \
+# 	  rocminfo               \
+# sudo usermod -aG render,video "$USER"
